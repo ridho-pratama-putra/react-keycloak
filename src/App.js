6e3ec63321b508ea23f6b235f2d65c05530858e5
ridho <ridho.putra@btpn.com>
2022-01-05 01:01:10 +0700

    integrate with keycloak

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
+import React from "react";
+import { ReactKeycloakProvider } from "@react-keycloak/web";
+import keycloak from "./KeycloakConfig"
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Nav from "./components/Nav";
 import WelcomePage from "./pages/Home";
 import SecuredPage from "./pages/SecuredPage";
+import PrivateRoute from "./helpers/PrivateRoute";
 
 function App() {
     return (
         <div>
-            <Nav/>
-            <BrowserRouter>
-                <Routes>
-                    <Route exact path="/" element={<WelcomePage/>}/>
-                    <Route path="/secured" element={<SecuredPage/>}/>
-                </Routes>
-            </BrowserRouter>
+            <ReactKeycloakProvider authClient={keycloak}>
+                <Nav />
+                <BrowserRouter>
+                    <Routes>
+                        <Route exact path="/" element={<WelcomePage />} />
+                        <Route
+                            path="/secured"
+                            element={
+                                <PrivateRoute>
+                                    <SecuredPage />
+                                </PrivateRoute>
+                            }
+                        />
+                    </Routes>
+                </BrowserRouter>
+            </ReactKeycloakProvider>
         </div>
     );
 }
--- src/KeycloakConfig.js
+++ src/KeycloakConfig.js
@@ -2,7 +2,7 @@ import Keycloak from "keycloak-js";
 const keycloak = new Keycloak({
     url: "http://localhost:8080/auth/",
     realm: "oauth2-demo",
-    resource: "react-login-client",
+    clientId: "react-login-client",
     // sslRequired: "external",
     // publicClient: true,
     // confidentialPort: 0
